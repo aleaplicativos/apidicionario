b797a5da7c872fe634fcaf8fec3fd2b094c5e88d
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-06-16 10:49:02 -0300

    Update apidicionary.php

--- apidicionary.php
+++ apidicionary.php
@@ -3,7 +3,7 @@ function consulta($fword)
     {
 		$ch = curl_init();
 		//chama o xml da API.
-		curl_setopt($ch, CURLOPT_URL, "http://dicionario-aberto.net/search-xml/".$fword);
+		curl_setopt($ch, CURLOPT_URL, "http://dicionario-aberto.net/search-json/".$fword);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$result = curl_exec($ch);
